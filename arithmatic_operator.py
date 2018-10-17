b73d99dedf1df9e6fc08471e21611a2ebf0320b2
Nishikesh Kardak <39300717+nishikeshKardak@users.noreply.github.com>
2018-10-17 15:33:39 +0530

    Create arithmatic_operator.py

---
+++ arithmatic_operator.py
@@ -0,0 +1,45 @@
+#Task 
+#Read two integers from STDIN and print three lines where:
+
+#1.The first line contains the sum of the two numbers.
+#2.The second line contains the difference of the two numbers (first - second).1.
+#3.The third line contains the product of the two numbers.
+
+#Input Format
+#The first line contains the first integer, . The second line contains the second integer, .
+
+#Constraints
+# 1 <= a <= 10^5
+# 1 <= b <= 10^5
+
+
+#Output Format
+#Print the three lines as explained above.
+
+#Sample Input 0
+#3
+#2
+
+#Sample Output 0
+#5
+#1
+#6
+
+#Explanation 0
+#3 + 2 = 5
+#3 - 1 = 2
+#3 * 2 = 6
+
+
+#The code is here
+
+a = int(input())
+b = int(input())
+
+
+print(a+b)
+print(a-b)
+print(a*b)
+
+ 
+
